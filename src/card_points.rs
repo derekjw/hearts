1578c5e873f85c26816991715272a5e929a83be7
Derek Williams <derek@fyrie.net>
2015-10-19 16:11:25 +0100

    Update card_points.rs

--- src/card_points.rs
+++ src/card_points.rs
@@ -7,20 +7,20 @@ pub struct CardPoints {
 }
 
 impl CardPoints {
-	pub fn new(card: Card, points: i32) -> CardPoints {
-		CardPoints { card: card, points: points }
-	}
+    pub fn new(card: Card, points: i32) -> CardPoints {
+        CardPoints { card: card, points: points }
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-	use card::*;
+    use card::*;
 
     #[test]
     fn equality() {
-    	let card = Card::new(Suit::Heart, Rank::Ace);
-    	assert_eq!(CardPoints::new(card, 1), CardPoints::new(card, 1));
+        let card = Card::new(Suit::Heart, Rank::Ace);
+        assert_eq!(CardPoints::new(card, 1), CardPoints::new(card, 1));
     }
 }

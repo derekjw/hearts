b6a5886480523ac0bf379b9f646e8d16f8aeecbe
Derek Williams <derek@nebvin.ca>
2015-10-29 08:45:53 +0000

    cleanup and notes

--- src/card_strategy/defensive_card_strategy.rs
+++ src/card_strategy/defensive_card_strategy.rs
@@ -12,6 +12,7 @@ pub struct DefensiveCardStrategy;
 /*
     Play 2 of clubs if in hand.
     If going to win a deal, do so with highest ranking card.
+    Need strategy for winning negetive penalty hands
 */
 impl DefensiveCardStrategy {
     fn score_card<'a>(card: &'a Card, game_status: &'a GameStatus, player_name: &PlayerName) -> (i32, i32, i32, i32) {
@@ -59,14 +60,14 @@ impl CardStrategy for DefensiveCardStrategy {
 
     fn pass_cards<'a>(&mut self, game_status: &'a GameStatus) -> Vec<&'a Card> {
         // Need to order cards by potential to win lowest penalty hands
-        let mut initial_hand: Vec<&'a Card> = game_status.my_initial_hand.iter().collect();
+        let mut initial_hand: Vec<&Card> = game_status.my_initial_hand.iter().collect();
         initial_hand.reverse();
         initial_hand.into_iter().take(3).collect()
     }
 
     fn play_card<'a>(&mut self, game_status: &'a GameStatus, player_name: &PlayerName) -> &'a Card {
         let current_suit = game_status.my_in_progress_deal.as_ref().and_then(|deal| deal.suit);
-        let mut valid_cards: Vec<&'a Card> = game_status.my_current_hand.iter().filter(|card| Some(card.suit) == current_suit).collect();
+        let mut valid_cards: Vec<&Card> = game_status.my_current_hand.iter().filter(|card| Some(card.suit) == current_suit).collect();
 
         if valid_cards.is_empty() {
             valid_cards.extend(&game_status.my_current_hand);

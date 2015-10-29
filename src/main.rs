7903bfdd7f8e9ac0d6d3369f6cc0fe86aacf088a
Derek Williams <derek@nebvin.ca>
2015-10-29 11:05:44 +0000

    add cli options

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ extern crate log;
 extern crate env_logger;
 extern crate serde;
 extern crate serde_json;
+extern crate clap;
 
 mod card;
 mod player;
@@ -25,17 +26,30 @@ use player::PlayerName;
 use player::Password;
 use card_strategy::DefensiveCardStrategy;
 
+use clap::App;
+
 #[allow(dead_code)]
 fn main() {
     env_logger::init().unwrap();
 
-    let player_name = PlayerName::new("FlyingBirds");
-    let password = Password::new("mypassword");
+    let cli_options = App::new("hearts")
+        .version("0.0.1")
+        .author("Derek Williams <derek@nebvin.ca>")
+        .about("Plays hearts for RBS Code Comp Nov 2015")
+        .args_from_usage(
+            "-u --user=<USER> 'Sets the player name'
+             -p --password=<PASSWORD> 'Sets the player password'
+             -s --server=<SERVER> 'Sets the hearts server'")
+        .get_matches();
+
+    let player_name = PlayerName::new(cli_options.value_of("USER").unwrap());
+    let password = Password::new(cli_options.value_of("PASSWORD").unwrap());
+    let server = cli_options.value_of("SERVER").unwrap();
 
     info!("Start Game");
 
     // Settings.init();
-    let player = Player::new(player_name, password, "localhost:2015", DefensiveCardStrategy);
+    let player = Player::new(player_name, password, server, DefensiveCardStrategy);
     player.play();
 }
 

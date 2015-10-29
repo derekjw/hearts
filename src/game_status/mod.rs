ef2ebd94173028378c88401d38c75654e3545a0e
Derek Williams <derek@nebvin.ca>
2015-10-29 13:48:50 +0000

    add final game status

--- src/game_status/mod.rs
+++ src/game_status/mod.rs
@@ -156,4 +156,13 @@ mod tests {
         GameStatus::try_from(game_status_dto).unwrap();
     }
 
+    #[test]
+    fn read_json4() {
+        let mut game_status_file = File::open("gamestatus4.json").unwrap();
+        let mut game_status_string = String::new();
+        game_status_file.read_to_string(&mut game_status_string).unwrap();
+        let game_status_dto: GameStatusDto = serde_json::from_str(&game_status_string).map_err(Error::from).unwrap();
+        GameStatus::try_from(game_status_dto).unwrap();
+    }
+
 }
